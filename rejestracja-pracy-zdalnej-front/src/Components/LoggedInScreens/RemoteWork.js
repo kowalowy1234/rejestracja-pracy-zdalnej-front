5a7a4c7b0bc53c8fcf7bf3ec7c938f0a707f96b2
Kernos308 <jacekg917@wp.pl>
2022-04-13 21:04:03 +0200

    Dodano wyciaganie z bazy danych pracownika i jego czas pracy

--- rejestracja-pracy-zdalnej-front/src/Components/LoggedInScreens/RemoteWork.js
+++ rejestracja-pracy-zdalnej-front/src/Components/LoggedInScreens/RemoteWork.js
@@ -1,66 +1,87 @@
 import { Calendar } from 'primereact/calendar';
 import "primeicons/primeicons.css";
 import React, { useState } from 'react';
-import Profile from '../Profile';
 import axios from 'axios';
+import endpoints from '../../endpoints';
+import { Knob } from 'primereact/knob';
+import { Button } from 'primereact/button';
 
-// class Dane extends React.Component {
-//   state = {
-//     name: '',
-//     vorname: ''
-//   }
 
-//   componentDidMount() {
-//     axios.get(`http://127.0.0.1:8000/auth/users/me/`)
-//       .then(res => {
-//         const imie = res.data.first_name;
-//         this.setState({ persons });
-//       })
-//   }
+const DanePracownika = () => {
 
-//   render() {
-//     return (
-//       <ul>
-//         {
-//           this.state.persons
-//             .map(person =>
-//               <li key={person.id}>{person.name}</li>
-//             )
-//         }
-//       </ul>
-//     )
-//   }
-// }
+  const token = sessionStorage.getItem('token')
+  const [dane, setImie] = useState({imie: null, nazwisko: null,})
 
-// const Dane1 = () => {
-//   const token = sessionStorage.getItem('token')
-//   const admin = sessionStorage.getItem('admin')
-//   const kierownik = sessionStorage.getItem('kierownik')
 
-//   return (
-//     <div>{kierownik}</div>
-//   )
-// }
+    React.useEffect(() => {
+    axios.get(endpoints.currentUser, {
+      headers: {
+        'Authorization': `Token ${token}`}
+    })
+    .then(function (response){
+      sessionStorage.setItem('idPracownika', response.data.id)
+      setImie( () => ({
+        imie: `${response.data.first_name}`, 
+        nazwisko : `${response.data.last_name}`}))
+    }).catch(function(error){
+      console.log(error);
+    });
+  },[]);
+  return (
+    <>
+    {dane.imie} {dane.nazwisko}
+    </>
+  )
+}
+
+const Praca = () => {
+
+  const [dane, setPraca] = useState(null);
+  React.useEffect(() => {
+    axios.get(endpoints.remoteWork)
+    .then(function (response) {
+      setPraca(response.data);
+    }).catch(function(error){
+      console.log(error);
+    });
+  },[]);
+  const id = sessionStorage.getItem('idPracownika')
+  console.log(dane)
+  
+  if (!dane) return null;
+  const listaRekordow = dane.map((rekord) => (
+    rekord.idPracownika === Number(id) ?
+    (<p><b>Remaining: {rekord.minutyPozostalo}</b></p>) : null
+    ))
+    console.log(listaRekordow)
+    return (
+    <>
+    <div className='remaining'>
+          {listaRekordow[0]}
+    </div>
+    </>
+  )
+}
 
 
 const Name = () => {
 
   return (
       <div className='name'>
-          <i className="pi pi-user" style={{'fontSize': '2em'}}>Lorem Lorem</i>
+          <i className="pi pi-user" style={{'fontSize': '2em'}}>&nbsp;<DanePracownika/></i>
       </div>
   )
 }
 
 
-const Remaining = () => {
+// const Remaining = () => {
 
-  return (
-      <div className='remaining'>
-          <p><b>Remaining: </b></p>
-      </div>
-  )
-}
+//   return (
+//       <div className='remaining'>
+//           <p><b>Remaining: <Praca/></b></p>
+//       </div>
+//   )
+// }
 
 const Calendarz = () => {
 
@@ -89,18 +110,55 @@ const Calendarz = () => {
   );
 }
 
+const Licznik = () => {
+
+  const [counter, setCounter] = useState(60);
+  const [value1, setValue1] = useState(1);
+  const [disabledIncrementBtn, setDisabledIncrementBtn] = useState(false);
+  const [disabledDecrementBtn, setDisabledDecrementBtn] = useState(true);
+
+  React.useEffect(() => {
+    const timer =
+      value1 > 0 && setInterval(() => setValue1(value1 + 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
+
+  const increment = () => {
+    const value = value1 + 1;
+    setValue1(value);
+    setDisabledIncrementBtn(value === 100);
+    setDisabledDecrementBtn(false);
+  }
+
+const decrement = () => {
+    const value = value1 - 1;
+    setValue1(value);
+    setDisabledIncrementBtn(false);
+    setDisabledDecrementBtn(value === 0);
+  }
+
+  return (
+    <div className="licznik">
+          <Knob value={value1} size={150} readOnly />
+          <Button label="Increment" onClick={increment} className="mr-2" disabled={disabledIncrementBtn} />
+          <Button label="Decrement" onClick={decrement} disabled={disabledDecrementBtn} />
+          Countdown: {counter}
+    </div>
+  )
+}
+
 const RemoteWork = () => {
   return (
     <div className="screen-container">
       <h1>Ekran pracy zdalnej</h1>
       <div className='podkont'>
-
         <Name/>
-        <Remaining/>
+        <Praca/>
       </div>
       <div className='kalendarz'>
         <Calendarz/>
       </div>
+      <Licznik/>
     </div>
   )
 }
---
+++ rejestracja-pracy-zdalnej-front/src/endpoints.js
@@ -0,0 +1,15 @@
+
+const host = "http://127.0.0.1:8000/auth/";
+
+const endpoints = {
+  employees: `${host}users/`,
+  companies : `${host}firma/`,
+  remoteWorkRecord : `${host}zapis-pracy/`,
+  remoteWork : `${host}praca`,
+  login: `${host}token/login/`,
+  logout: `${host}token/logout/`,
+  currentUser: `${host}users/me/`,
+  usersAndRegister: `${host}users/`,
+}
+
+export default endpoints;
