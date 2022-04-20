eace9f1d81edee29471928b666cbfc12069c9a5a
Piotr <kowalowy1234@gmail.com>
2022-04-20 22:42:53 +0200

    Stworzono makietę ekranu wyznaczania pracy zdalnej

--- rejestracja-pracy-zdalnej-front/src/Components/LoggedInMainPage.js
+++ rejestracja-pracy-zdalnej-front/src/Components/LoggedInMainPage.js
@@ -31,7 +31,7 @@ const LoggedInMainPage = (props) => {
           <Route exact path="/" element={<MainPage />} />
           <Route exact path="dodaj_pracownika" element={<MainPage />} />
           <Route exact path="ekran_pracy_zdalnej" element={<RemoteWork />} />
-          <Route exact path="wyznacz_prace" element={<AssignRemoteWork />} />
+          <Route exact path="wyznacz_prace" element={<AssignRemoteWork token={sessionStorage.getItem('token')} companyId={sessionStorage.getItem('companyId')}/>} />
           <Route exact path="statystyki" element={<Stats token={sessionStorage.getItem('token')} companyId={sessionStorage.getItem('companyId')}/>} />
         </Routes>
       </div>
--- rejestracja-pracy-zdalnej-front/src/Components/LoggedInScreens/AssignRemoteWork.js
+++ rejestracja-pracy-zdalnej-front/src/Components/LoggedInScreens/AssignRemoteWork.js
@@ -1,16 +1,157 @@
+import { MultiSelect } from 'primereact/multiselect';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import endpoints from '../../endpoints';
+import { Calendar } from 'primereact/calendar';
+import { Button } from 'primereact/button';
+import { InputNumber } from 'primereact/inputnumber';
 
+const AssignRemoteWork = (props) => {
 
-const AssignRemoteWork = () => {
+  const [users, setUsers] = useState([])
+
+  const [allUsers, setAllUsers] = useState([])
+
+  const today = new Date();
+  const tomorrow = new Date();
+  today.setDate(today.getDate() + 1)
+  tomorrow.setDate(tomorrow.getDate() + 2);
+
+  const [selectedDates, setSelectedDates] = useState([today, tomorrow]);
+
+  const [selectedTime, setSelectedTime] = useState({hours: 0, minutes: 1});
+
+  const diffTime = Math.abs(selectedDates[1] - selectedDates[0]);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; 
+
+  // pobieranie przefiltrowanej listy pracowników tylko z danej firmy
+  useEffect(() => {
+
+    axios.get(endpoints.usersAndRegister, {
+      headers: {
+        'Authorization': `Token ${props.token}`
+      }
+    }).then(response => {
+
+      const employees = response.data;
+      
+      let employeesFiltered = employees.filter(user => {
+        return (Number(user.firma) === Number(props.companyId)) && (user.is_superuser === false);
+      });
+
+      let employeesMapped = employeesFiltered.map(user => (
+        {
+          label: `${user.first_name} ${user.last_name} | ${user.email}`,
+          value: user.id,
+        }
+      ))
+      setAllUsers(employeesMapped);
+
+    }).catch(error => {
+      console.log(error);
+    })
+
+  }, [])
+
   return (
     <div className="screen-container">
-      <h1>Ekran wyznaczania pracy zdalnej</h1>
+      <h2 style={{marginBottom: '20px'}}><i className='pi pi-desktop' style={{fontSize: '20px', marginRight: '10px'}}></i>Ekran wyznaczania pracy zdalnej</h2>
+      <hr style={{width: '100%', backgroundColor: 'rgb(233, 233, 233)', border: '1px solid rgb(233, 233, 233)', marginBottom: '20px'}}></hr>
       {/* Lista pracowników */}
-
+      <div className='stats-header assign-header'>
+        <label htmlFor="select" style={{paddingBottom: '10px'}}>Pracownicy</label>
+        <MultiSelect 
+          inputId='select'
+          selectedItemsLabel={`Wybrano ${users.length} pracowników`} 
+          maxSelectedLabels={1} filter={true} 
+          placeholder="Pracownicy" 
+          value={users} 
+          options={allUsers} 
+          onChange={(e) => setUsers(e.value)}   
+        />
+      </div>
       {/* Pola wyboru daty */}
-
-      {/* Pole ilości godzin */}
-      
-      {/* Przyciski zatwierdź i anuluj */}
+      <div className='stats-header assign-header' style={{
+        flexDirection: 'column',
+        alignItems: 'center'
+        }}>
+        <div className='timeSelect'>
+          <div style={{alignSelf: 'flex-start',display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px'}}>
+            <label htmlFor="calendar">Okres pracy</label>
+            <Calendar 
+              inputId='calendar'
+              dateFormat='yy-m-d'
+              appendTo='self'
+              selectionMode="range"
+              minDate={today}
+              value={selectedDates}
+              onChange = {(e) => setSelectedDates(e.value)}
+            />
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px'}}>
+            <label htmlFor="verticalHours">Ilość godzin</label>
+            <InputNumber
+              inputId='verticalHours' 
+              style={{width: '4rem'}}
+              name='hours'
+              showButtons buttonLayout="vertical"
+              min={0}
+              max={ diffDays * 8 }
+              value={selectedTime.hours}
+              onChange={(e) => setSelectedTime(prevTime => {
+                if (e.value === diffDays * 8) {
+                  return {
+                    ...prevTime,
+                    hours: e.value,
+                    minutes: 0,
+                  };
+                } else if (e.value === 0) {
+                  return {
+                    ...prevTime,
+                    hours: e.value,
+                    minutes: 1,
+                  }
+                } else {
+                  return {
+                    ...prevTime,
+                    hours: e.value,
+                  };
+                }
+              })}
+                />
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '10px'}}>
+            <label htmlFor="verticalMinutes">Ilość Minut</label>
+            <InputNumber 
+              inputId='verticalMinutes'
+              style={{width: '4rem'}}
+              name='minutes'
+              showButtons buttonLayout="vertical"
+              min={selectedTime.hours === diffDays * 8 ? 0 : selectedTime.hours > 0 ? 0 : 1}
+              disabled={selectedTime.hours === diffDays * 8 ? true : false}
+              max={59}
+              value={selectedTime.minutes}
+              onChange={(e) => setSelectedTime(prevTime => (
+                {
+                  ...prevTime,
+                  minutes: e.value
+                }
+              ))}
+            />
+          </div>
+          <div className='buttons'>
+            <Button 
+              label='Zatwierdź'
+              icon="pi pi-check"
+              />
+            <Button 
+              className='p-button-danger'
+              label='Anuluj'
+              icon="pi pi-times"
+              />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
