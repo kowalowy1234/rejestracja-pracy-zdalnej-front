f835da72260809a84f41b02d84e1f647ffd86813
Piotr <kowalowy1234@gmail.com>
2022-05-26 09:39:12 +0200

    debugging

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,11 +1,12 @@
 
 const host = "https://deploy-test-praca.herokuapp.com/auth/";
+// const host = "http://127.0.0.1:8000/auth/";
 
 const endpoints = {
   employees: `${host}users/`,
   companies : `${host}firma/`,
-  remoteWorkRecord : `${host}zapis-pracy/`,
-  remoteWork : `${host}praca/`,
+  remoteWorkRecord : `${host}zapis-pracy`,
+  remoteWork : `${host}praca`,
   login: `${host}token/login/`,
   logout: `${host}token/logout/`,
   currentUser: `${host}users/me/`,
